46092abeb413fef48b50bcfc2ab87c3e06d26aa3
Arsene <catalincik123@gmail.com>
2023-09-04 13:27:38 +0300

    create function findEven

---
+++ script.js
@@ -0,0 +1,12 @@
+function findEven(arr){
+    let evenArr = []
+    for(let i = 0;i < arr.length;i++){
+        if(arr[i] % 2 === 0){
+            evenArr.push(arr[i])
+        }
+    }
+    console.log(evenArr)
+}
+
+arr = [1,2,3,4,5,6]
+findEven(arr)
